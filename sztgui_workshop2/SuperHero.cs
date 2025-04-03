2d5e796897771601c7458377f0cb2fc56675c552
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-04-03 11:06:59 +0200

    SuperHero class done

---
+++ sztgui_workshop2/SuperHero.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sztgui_workshop2
+{
+    public class SuperHero : INotifyPropertyChanged
+    {
+		private string name;
+
+		private int strength;
+
+		private int speed;
+
+		private bool isMutant;
+
+		public bool IsMutant
+		{
+			get { return isMutant; }
+			set { isMutant = value; OnPropertyChanged(); }
+		}
+
+
+		public int Speed
+		{
+			get { return speed; }
+			set { speed = value; OnPropertyChanged(); }
+		}
+
+
+		public int Strength
+		{
+			get { return strength; }
+			set { strength = value; OnPropertyChanged(); }
+		}
+
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; OnPropertyChanged(); }
+		}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?
+				.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+    }
+}
